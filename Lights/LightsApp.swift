20fb17241db2a012331b7381dfc8c07c7da031ba
Alexander Makarov <fsbisan@mail.ru>
2021-10-27 21:19:44 +0500

    Initial Commit

---
+++ Lights/LightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightsApp.swift
+//  Lights
+//
+//  Created by Александр Макаров on 27.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct LightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
